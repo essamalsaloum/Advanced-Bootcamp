38a82486bb8bc716d81a1eebdc38a31e5120a9a4
ewok187 <k.wessalowski@gmx.de>
2018-02-16 23:16:40 +0100

    react d3

--- React/recipe-app/src/Navbar.js
+++ React/recipe-app/src/Navbar.js
@@ -1,13 +1,22 @@
 import React, {Component} from "react";
+import PropTypes from "prop-types";
 import "./Navbar.css";
 
 class Navbar extends Component {
+    static defaultProps = {
+        onNewRecipe() {}
+    }
+
+    static propTypes = {
+        onNewRecipe: PropTypes.func
+    }
+
     render() {
         return (
             <header>
                 <h2><a> Recipe App </a></h2>
                 <nav>
-                    <li><a href="">New Recipe</a></li>
+                    <li><a onClick={this.props.onNewRecipe}>New Recipe</a></li>
                     <li><a href=""></a>Home</li>
                     <li><a href=""></a>About</li>
                     <li><a href=""></a>Contact Us</li>
